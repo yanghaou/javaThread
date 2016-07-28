1c7d04d702e12024d2f523f2c2e2e01f368a0829
杨豪 <yang.hao@whaley.cn>
2016-07-28 19:56:54 +0800

    first commit

---
+++ src/main/java/char1/t3/MyThread.java
@@ -0,0 +1,13 @@
+package char1.t3;
+
+public class MyThread extends Thread {
+
+	private int count=5;
+
+	@Override
+	synchronized public void run() {
+		super.run();
+		count--;
+		System.out.println("由"+this.currentThread().getName()+" 计算count="+count);
+	}
+}
